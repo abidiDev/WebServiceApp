6488c6361c03cdd8f9b0eda9f6695d6e3ec1e09d
WaelAbidi1 <wael.abidi@esprit.tn>
2022-09-24 15:59:22 +0100

    helloWebService

---
+++ src/main/java/ressources/Hello.java
@@ -0,0 +1,36 @@
+package ressources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("hello")
+public class Hello {
+	public Hello() {
+	}
+	//ressource 1
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response sayHello() {
+		return Response.status(200).entity("hello").build();
+	}
+	//ressource accessible with queryParam
+	@Path("hello2")
+	@GET()
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response sayHello2(@QueryParam(value = "FirstName") String prenom, @QueryParam(value = "LastName")String nom) {
+		
+		return Response.status(200).entity("hello"+nom+""+prenom).build();	
+		}
+	//ressource accessible with pathParam
+	@Path("hello3/{FirstName}/{LastName}")
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response sayHello3(@PathParam(value="FirstName") String nom,@PathParam(value="LastName") String prenom) {
+		return Response.status(200).entity("hello"+nom+""+prenom).build();
+	}
+}
---
+++ src/main/java/ressources/RestActivator.java
@@ -0,0 +1,10 @@
+package ressources;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("Rest")
+
+public class RestActivator  extends Application {
+
+}
